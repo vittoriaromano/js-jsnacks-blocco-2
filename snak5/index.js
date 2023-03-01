eff66cfbc9e3151f1e5560e29955d61147eb29ec
vittoriaromano <vittoria5005@gmail.com>
2023-03-01 16:14:23 +0400

    snak5

---
+++ snak5/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function pariDispari(num){
+    if(num % 2 == 0 ){
+        return true;
+
+    }else{
+        return false;
+    }
+}
+console.log(pariDispari(3));
+console.log(pariDispari(4));
+
+
+// pariDispari(''){
+//     let random = prompt('numero');
+//     console.log(random)
+//     console.log(pariDispari)
+// }
